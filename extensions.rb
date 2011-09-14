a4e8db4489cc9107ffd9c6301a80c189ab2ad355
Roger Braun <davinelulinvega@gmail.com>
2011-09-14 16:45:41 +0200

    switch to hue

--- extensions.rb
+++ extensions.rb
@@ -1,73 +1,18 @@
 module ChunkyPNG::Color
-  def self.to_rg(pixel)
-    r, g, b = r(pixel), g(pixel), b(pixel)
-    [r / (r + g + b).to_f, g / (r + b + g).to_f]
-  end
-
   def self.hue(pixel)
     r, g, b = r(pixel), g(pixel), b(pixel)
     return 0 if r == b and b == g 
-    180 / Math::PI * Math.atan2(2 * r - g -b, Math.sqrt(3) * (g - b)) 
+    180 / Math::PI * Math.atan2((2 * r) - g -b, Math.sqrt(3) * (g - b)) 
   end
 
   def self.distance(pixel, poxel)
-    # x, y = to_rg(pixel), to_rg(poxel)
-    #a, b = x[0] - y[0], x[1] - y[1]
-
-    # Divide by 1.41 to get results from 0 to 1
-    #Math.sqrt((a * a) + (b * b)) / Math.sqrt(2)
     (hue(pixel) - hue(poxel)).abs
   end
 end
 
-module Neighbors
-  def neighbors(x, y = nil)
-    unless y
-      y = x / width
-      x = x - y * width
-    end
-    candidates = ((x - 1)..(x + 1)).to_a.product(((y - 1)..(y + 1)).to_a)    
-    candidates.select{|el| include_point? el} - [[x, y]]
-  end
-end
-
 module SelectiveColor
-  def to_selective_color(color, avoid = nil, spread = nil)
-    colored = pixels.map do |pixel| 
-      too_far = color.inject(false){|r, (c, d)| r or (ChunkyPNG::Color.distance(c, pixel) > d)}
-      too_close = avoid && avoid.inject(false){|r, (c, d)| r or (ChunkyPNG::Color.distance(c, pixel) < d)}
-      if too_far or too_close
-        false
-      else
-        true
-      end
-    end
-
-    if spread then
-      modified = true
-      while modified do
-        modified = false
-        pixels.each_with_index do |pixel, i|
-          if colored[i]
-            neighbors(i).each do |(x,y)|
-              unless colored[x + (y * width)]
-                other_pixel = get_pixel(x, y)
-                if ChunkyPNG::Color.distance(pixel, other_pixel) < spread
-                  modified = (modified || 0) + 1
-                  colored[x + (y * width)] = true
-                end
-              end
-            end
-          end
-        end    
-        puts "Modified #{modified} pixels"
-      end 
-    end
-
-    colored.each_with_index do |color, i|
-      pixels[i] = ChunkyPNG::Color.to_grayscale(pixels[i]) unless color
-    end
-
-    return self
+  def to_selective_color!(reference, delta)
+    pixels.map!{|pixel| ChunkyPNG::Color.distance(pixel, reference) > delta ? ChunkyPNG::Color.to_grayscale(pixel) : pixel}
+    self
   end
 end
--- selective_color.rb
+++ selective_color.rb
@@ -1,17 +1,14 @@
 require "chunky_png"
 require "./extensions.rb"
-require "pry"
-
 
 image = ChunkyPNG::Image.from_file("input.png")
 
-image.extend(SelectiveColor, Neighbors)
+image.extend(SelectiveColor)
 
+#keep = ChunkyPNG::Color.rgb(221,57,42)
+#keep = ChunkyPNG::Color.rgb(152,216,56)
+keep = ChunkyPNG::Color.rgb(0,125,209)
 
-keep = [[ChunkyPNG::Color.rgb(221,57,42), 10]]#[[ChunkyPNG::Color.rgb(152,216,56), 0.1]]
-
-avoid = nil #[[ChunkyPNG::Color.rgb(255,229,62), 0.05]]
-
-image.to_selective_color(keep)
+image.to_selective_color!(keep, 15)
 
 image.save("output.png")
