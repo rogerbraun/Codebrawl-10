5db45290963f274ee95600dee74b058a2c3be147
Roger Braun <davinelulinvega@gmail.com>
2011-09-14 16:31:17 +0200

    split files

--- selective_color.rb
+++ selective_color.rb
@@ -1,45 +1,17 @@
 require "chunky_png"
+require "./extensions.rb"
+require "pry"
 
-module ChunkyPNG::Color
-  def self.to_rg(pixel)
-    r, g, b = r(pixel), g(pixel), b(pixel)
-    [r / (r + g + b).to_f, g / (r + b + g).to_f]
-  end
-
-  def self.distance(pixel, poxel)
-    x, y = to_rg(pixel), to_rg(poxel)
-    a, b = x[0] - y[0], x[1] - y[1]
-
-    # Divide by 1.41 to get results from 0 to 1
-    Math.sqrt((a * a) + (b * b)) / Math.sqrt(2)
-  end
-end
-
-module SelectiveColor
-  def to_selective_color(color, avoid = nil)
-    pixels.map! do |pixel| 
-      too_far = color.inject(false){|r, (c, d)| r or (ChunkyPNG::Color.distance(c, pixel) > d)}
-      too_close = avoid && avoid.inject(false){|r, (c, d)| r or (ChunkyPNG::Color.distance(c, pixel) < d)}
-      if too_far or too_close
-        ChunkyPNG::Color.to_grayscale(pixel)
-      else
-        pixel
-      end
-    end
-
-    return self
-  end
-end
 
 image = ChunkyPNG::Image.from_file("input.png")
 
-image.extend(SelectiveColor)
+image.extend(SelectiveColor, Neighbors)
 
 
-keep = [[ChunkyPNG::Color.rgb(152,216,56), 0.1]]
+keep = [[ChunkyPNG::Color.rgb(221,57,42), 10]]#[[ChunkyPNG::Color.rgb(152,216,56), 0.1]]
 
-avoid = [[ChunkyPNG::Color.rgb(255,229,62), 0.05]]
+avoid = nil #[[ChunkyPNG::Color.rgb(255,229,62), 0.05]]
 
-image.to_selective_color(keep, avoid)
+image.to_selective_color(keep)
 
 image.save("output.png")
